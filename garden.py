ae0f3d35db129f0e85c9c4fcd5a1d9030319e84c
YehorA <artushinegor@gmail.com>
2025-06-01 15:28:34 +0200

    Season count, probability for trees to die from illness, and guaranteed death from old age

--- garden.py
+++ garden.py
@@ -1,6 +1,9 @@
+import random
+
 class Garden:
     def __init__(self):
         self.trees = []
+        self.season = 1
 
     def add_tree(self, tree):
         self.trees.append(tree)
@@ -14,10 +17,31 @@ class Garden:
         print()
 
     def simulate_season(self):
+        print(f"\n--- SEASON {self.season} ---")
+        self.season += 1
+        dead_trees = []
+
         for tree in self.trees:
             tree.check_for_disease()
-            tree.age+=1
-            self.show_tree(tree)
+            tree.age += 1
+
+            if tree.has_disease and random.random() < 0.2:
+                print(f"{tree.__class__.__name__} (age {tree.age}) has died due to disease.")
+                print()
+                dead_trees.append(tree)
+
+            elif tree.age > 30:
+                print(f"{tree.__class__.__name__} (age {tree.age}) has died of old age.")
+                print()
+                dead_trees.append(tree)
+
+            else:
+                self.show_tree(tree)
+
+        #remove dead tree
+        for dead in dead_trees:
+            self.trees.remove(dead)
+
 
             
         
--- test_.py
+++ test_.py
@@ -48,7 +48,7 @@ class TestCherryTree(unittest.TestCase):
         tree.has_disease = True
         self.assertEqual(tree.get_yield(), 80 * 0.3)
 
-#-----------------------------------------------------------------------------
+#GARDEN TESTING-----------------------------------------------------------------------------
 
 class TestGarden(unittest.TestCase):
     def test_simulate_season_increments_age(self):
@@ -62,6 +62,37 @@ class TestGarden(unittest.TestCase):
 
         self.assertEqual(apple.age, 7)
         self.assertEqual(pear.age, 8)
+    
+    def test_old_age_kills_tree(self):
+        tree = AppleTree(age=30)
+        garden = Garden()
+        garden.add_tree(tree)
+        garden.season = 0
+        garden.simulate_season()
+        self.assertEqual(len(garden.trees), 0)
+
+    #simulates a garden with 100 ill trees where at least one will die with very high probability after 20 seasons
+    def test_disease_kills_some_trees_in_population(self):
+        garden = Garden()
+        num_trees = 100
+
+        starting_age = 5
+
+        for _ in range(num_trees):
+            tree = AppleTree(starting_age)
+            tree.has_disease = True
+            garden.add_tree(tree)
+
+        garden.season = 0;
+
+        for _ in range(20):
+            garden.simulate_season()
+
+            for tree in garden.trees:
+                tree.has_disease = True
+
+        self.assertLess(len(garden.trees), num_trees)
+
 
 
 if __name__ == '__main__':
